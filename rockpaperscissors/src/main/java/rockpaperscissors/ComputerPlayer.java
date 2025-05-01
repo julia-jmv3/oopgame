d552d47ee5df0e74ecd1a707286fe8ac73072e3c
JuliaVennall <j.vennall@corecomtechacademy.co.uk>
2025-05-01 09:21:38 +0100

    adding constructor to computer player class

--- rockpaperscissors/src/main/java/rockpaperscissors/ComputerPlayer.java
+++ rockpaperscissors/src/main/java/rockpaperscissors/ComputerPlayer.java
@@ -1,4 +1,15 @@
 package rockpaperscissors;
 
+import java.util.Random;
+
 public class ComputerPlayer extends Player{
+
+    private Random random;
+
+    public ComputerPlayer(String name) {
+        super(name);
+        random = new Random();
+    }
+
+
 }
