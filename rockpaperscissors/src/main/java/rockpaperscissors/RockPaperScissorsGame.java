9e96b58a936b8da0552a908c43133fb0708e967d
JuliaVennall <j.vennall@corecomtechacademy.co.uk>
2025-05-01 10:35:43 +0100

    implementing game interface

--- rockpaperscissors/src/main/java/rockpaperscissors/RockPaperScissorsGame.java
+++ rockpaperscissors/src/main/java/rockpaperscissors/RockPaperScissorsGame.java
@@ -1,4 +1,27 @@
 package rockpaperscissors;
 
-public class RockPaperScissorsGame {
+public class RockPaperScissorsGame implements Game{
+
+    //players
+    private UserPlayer user;
+    private ComputerPlayer computer;
+
+    //constructor
+    public RockPaperScissorsGame(UserPlayer user, ComputerPlayer computer){
+        this.user = user;
+        this.computer = computer;
+    }
+
+
+    @Override
+    public void playGame(Player player) {
+        rockpaperscissorsoptions turn1 = user.getTurn();
+        rockpaperscissorsoptions turn2 = computer.getTurn();
+
+        //output choices made by each player
+        System.out.println(user.getName()+ " said " + turn1);
+        System.out.println(computer.getName()+ " said " + turn2);
+
+        //add more code here
+    }
 }
